985b17969db0fc8f0f8eb78d1a1f98bfa56dc432
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-02-22 11:33:13 +0100

    Commentaire ajouté pour le cycle de vie d'une activitée.

--- app/src/main/java/com/example/completenavigation/MainActivity.kt
+++ app/src/main/java/com/example/completenavigation/MainActivity.kt
@@ -12,6 +12,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var bottomNav: BottomNavigationView
 
+    /**
+     * Dans la méthode onCreated : les opérations servent à mettre en place
+     * l'interface graphique, initialiser les variables,
+     * configurer les listeners ou à se connecter au modèle
+     *
+     * l'activitée est créée mais l'utilisateur ne la
+     * voit pas encore et ne peut pas intéragir avec
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,6 +49,59 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    /**
+     * cette méthode est appelée par le système lorsque
+     * l'activité entre dans l'état Started
+     *
+     * L'interface graphique devient visible à l'utilisateur,
+     * mais il ne peut pas encore intéragir avecles différents éléments graphiques
+     */
+    override fun onStart() {
+        super.onStart()
+    }
+
+    /**
+     * Cette méthode est appelée lorsque l'activité entre dans l'état Resumed.
+     * L'activité devient entièrement opérationnelle.
+     *
+     * L'uitilisateur peut utiliser l'application et
+     * cliquer sur les différents éléments graphiques
+     */
+    override fun onResume() {
+        super.onResume()
+    }
+
+    /**
+     * Cette méthode est appelée lorsque l'activité
+     * entre dans l'état Paused.
+     * Une Activity passe par exemple dans
+     * l’état Paused lorsqu’une AlertDialog est affichée
+     */
+    override fun onPause() {
+        super.onPause()
+    }
+
+    /**
+     * Cette méthode est appelée lorsque l'activité
+     * entre dans l'état Stopped. Par exemple,
+     * lorsqu'une nouvelle activité est démarrée,
+     * l'activité appelante va se retrouver dans cet état
+     */
+    override fun onStop() {
+        super.onStop()
+    }
+
+    /**
+     * Cette méthode est appelée lorsque
+     * l'activité est arrêtée. Par exemple,
+     * ce peut être après avoir appelée la
+     * méthode finish(), ou si le système
+     * décide d'arrêter l'activité pour libérer de la mémoire.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     private fun loadFragment(fragment: Fragment) {
